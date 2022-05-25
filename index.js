6e71b18496278b247d45ededae146f2cbb5d2033
Brian Allen <vecter1180@gmail.com>
2022-05-25 12:34:44 -0400

    commit tree 044e1576bdf8a1316310ada5598a764eb19cc34b

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+
+function saturdayFun(activity = 'roller-skate'){
+   return  (`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork(action = 'go to the office'){
+   return (`This Monday, I will ${action}.`)
+}
+
+function wrapAdjective(flaire = '*'){
+   return function(argu1 = "special") {
+      return `You are ${flaire}${argu1}${flaire}!`
+   }
+}
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,4 +1,4 @@
-describe("index.js", () => {
+  describe("index.js", () => {
   describe("defines saturdayFun function declaration as specified", function() {
     it("function exists", function() {
       expect(saturdayFun).to.exist
